89e88f5a8d23b6821865b947274248da568d3084
ajpawlicki <aloysiuspaw@gmail.com>
2017-02-13 19:20:25 -0800

    Finish pseudoclassical inheritance style

--- src/blueDancer.js
+++ src/blueDancer.js
@@ -1,16 +1,12 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var makeBlueDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-  // this.blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-  // this.temp = new makeDancer(top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  // return blinkyDancer;
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.step = function() {
+makeBlueDancer.prototype = Object.create(makeDancer.prototype);
+makeBlueDancer.prototype.constructor = makeBlueDancer;
+makeBlueDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
   // this.step.bind(makeDancer);
